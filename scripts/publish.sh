38201ce6970c8d5e190696624ca0d286bb7fc0d1
kumarabd <abishekkumar92@gmail.com>
2021-04-19 20:33:11 +0530

    tar fixes
    Signed-off-by: kumarabd <abishekkumar92@gmail.com>

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,3 @@
-PREFIX="" # ask otto
 ARCH=$INPUT_ARCHITECTURE
 DISTRO=""
 OS=$INPUT_OS
@@ -17,16 +16,10 @@ if [ -z "$DISTRO" ]; then
   exit 1
 fi
 
-ASSET_NAME="nighthawk-$DISTRO-$ARCH-$INPUT_VERSION.tar.gz"
-
 printf "INFO\tBAZEL FOLDER\n"
 bazel info -c opt bazel-bin
 
 ROOT_FOLDER=$(bazel info -c opt bazel-bin)
-CLIENT_BINARY="$ROOT_FOLDER/nighthawk_client"
-SERVICE_BINARY="$ROOT_FOLDER/nighthawk_service"
-TEST_SERVER_BINARY="$ROOT_FOLDER/nighthawk_test_server"
-OUTPUT_TRANSFORM_BINARY="$ROOT_FOLDER/nighthawk_output_transform"
 
 # Optional personal access token for external repository
 TOKEN=$GITHUB_TOKEN
@@ -56,23 +49,22 @@ if ! type "tar" > /dev/null 2>&1; then
   exit 1;
 fi
 
-if ! tar -zcvf $ASSET_NAME $CLIENT_BINARY $SERVICE_BINARY $TEST_SERVER_BINARY $OUTPUT_TRANSFORM_BINARY; then
-    printf "ERROR\tUnable to create bundle\n"
-fi
-
-# Upload artifact
-GITHUB_API_URL="api.github.com"
-RELEASE_URL="https://$GITHUB_API_URL/repos/$INPUT_REPO/releases"
-RELEASE_UPLOAD_URL=$(curl -H "Authorization: token $TOKEN" $RELEASE_URL | jq -r '.[] | select(.tag_name == "'${INPUT_VERSION}'")' | jq -r .upload_url)
-pattern="{?"
-RELEASE_ASSET_URL="${RELEASE_UPLOAD_URL%$pattern*}"
-
-curl \
-    -X POST \
-    -H "Content-Type: application/tar" \
-    -H "Authorization: token $TOKEN" \
-    -H "Accept: application/vnd.github.v3+json" \
-    --data-binary @${ASSET_NAME} \
-    "$RELEASE_ASSET_URL?name=$ASSET_NAME"
-
+ASSET_SUFFIX="$DISTRO-$ARCH-$INPUT_VERSION.tar.gz"
+for binary in nighthawk_client nighthawk_service nighthawk_test_server nighthawk_output_transform
+do
+    if ! tar -zcvf $binary-$ASSET_SUFFIX --directory=$ROOT_FOLDER $binary; then
+        printf "ERROR\tUnable to create bundle\n"
+    fi
+    # Upload artifact
+    GITHUB_API_URL="api.github.com"
+    RELEASE_URL="https://$GITHUB_API_URL/repos/$INPUT_REPO/releases"
+    RELEASE_UPLOAD_URL=$(curl -H "Authorization: token $TOKEN" $RELEASE_URL | jq -r '.[] | select(.tag_name == "'${INPUT_VERSION}'")' | jq -r .upload_url)
+    pattern="{?"
+    RELEASE_ASSET_URL="${RELEASE_UPLOAD_URL%$pattern*}"
+    response=$(curl -X POST -H "Content-Type: application/tar" -H "Authorization: token $TOKEN" -H "Accept: application/vnd.github.v3+json" --data-binary @$binary-$ASSET_SUFFIX "$RELEASE_ASSET_URL?name=$binary-$ASSET_SUFFIX")
+    if ! response; then
+        printf "ERROR\tUnable to post the artifact\n"
+    fi
+    printf "INFO\tUploaded $binary succesfully!!\n"
+done
 printf "INFO\tAction ran succesfully!!\n"
