2fc4da48518bcc4eb3e1d06f3ef75ff896368bfd
Sunny Kumar <sunny514173@gmail.com>
2022-01-27 13:49:56 +0530

    shift floating menu button
    Signed-off-by: Sunny Kumar <sunny514173@gmail.com>

--- docs/assets/js/docs_nav.js
+++ docs/assets/js/docs_nav.js
@@ -3,11 +3,11 @@ const menuTray = document.getElementById("menuTray")
 document.querySelector('#toggle').addEventListener("click", () => {
    if (menuTray.style.visibility === 'hidden') {
        menuTray.style.visibility = 'visible';
-       document.getElementById("float-icon-arrow").classList.remove('fa-angle-double-up')
-       document.getElementById("float-icon-arrow").classList.add('fa-angle-double-down')
+       document.getElementById("float-icon-arrow").classList.remove('fa-angle-double-right')
+       document.getElementById("float-icon-arrow").classList.add('fa-angle-double-left')
    } else{
     menuTray.style.visibility = 'hidden';
-    document.getElementById("float-icon-arrow").classList.remove('fa-angle-double-down')
-       document.getElementById("float-icon-arrow").classList.add('fa-angle-double-up')
+    document.getElementById("float-icon-arrow").classList.remove('fa-angle-double-left')
+       document.getElementById("float-icon-arrow").classList.add('fa-angle-double-right')
    }
 })
